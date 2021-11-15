9f95aee781da8ce6d5ab722af76578e816049c62
Joel <jp@follett.com>
2021-11-15 18:32:17 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+
+document.getElementById("but1").onclick = function () 
+{
+    
+    let img = "../externaljs&switchcase/images.html/kalabhyrava.jpeg";
+    document.getElementById("kala").src = img;
+    document.getElementById("kala").style.display = "block";
+
+}
+
+document.getElementById("but2").onclick = function () 
+{
+   
+    let img = "../externaljs&switchcase/images.html/indu.jpeg";
+    document.getElementById("kala").src = img;
+    document.getElementById("kala").style.display = "block";
+}
+document.getElementById("but3").onclick = function () 
+{
+    let img = "../externaljs&switchcase/images.html/sherkhan.jpeg";
+    document.getElementById("kala").src = img;
+    document.getElementById("kala").style.display = "block";
+}
+document.getElementById("but4").onclick = function () 
+{
+    
+    let img = "../externaljs&switchcase/images.html/randevbilla.jpeg";
+    document.getElementById("kala").src = img;
+    document.getElementById("kala").style.display = "block";
+}
+document.getElementById("but5").onclick = function () 
+{
+    
+    let img = "../externaljs&switchcase/images.html/agora.jpeg";
+    document.getElementById("kala").src = img;
+    document.getElementById("kala").style.display = "block";
+}
+document.getElementById("but6").onclick = function () 
+{
+    
+    let img = "../externaljs&switchcase/images.html/sunil.jpeg";
+    document.getElementById("kala").src = img;
+    document.getElementById("kala").style.display = "block";
+}
+
